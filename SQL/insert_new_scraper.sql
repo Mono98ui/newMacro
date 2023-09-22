3c66c536e1932d4194f35816ab8cae83dca12c99
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2023-09-22 14:04:16 -0400

    add sec

--- SQL/insert_new_scraper.sql
+++
@@ -1,3 +0,0 @@
---Exemeple of command of insert a new process
-insert into t_status_process(process_name, status) values('process_fred', 0);
-insert into t_status_process(process_name, status) values('process_investing', 0);
